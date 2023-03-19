a9c9b900be2621d3db5a2f4007c4d5a17fe62fee
vilasrhegde <vilasrhegde@gmail.com>
2023-03-19 17:12:22 +0530

    added certificates

--- rename.py
+++ rename.py
@@ -16,7 +16,8 @@ import shutil
 for i in new_names:
     old=path+'/'+i
     new=path+'/'+new_names[i]
-    shutil.move(old,new)    import os
+    shutil.move(old,new)    
+import os
 path='C:/Users/vilas/Desktop/Important/vilasrhegde.github.io/images/test'
 dir_list = os.listdir(path)
  
@@ -34,7 +35,8 @@ import shutil
 for i in new_names:
     old=path+'/'+i
     new=path+'/'+new_names[i]
-    shutil.move(old,new)    import os
+    shutil.move(old,new)    
+import os
 path='C:/Users/vilas/Desktop/Important/vilasrhegde.github.io/images/test'
 dir_list = os.listdir(path)
  
